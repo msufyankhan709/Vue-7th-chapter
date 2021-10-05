b1ded1b63de32001aa88c02e4c4627ebff277180
M Sufyan Khan <msufyankhan709@gmail.com>
2021-10-05 17:50:15 +0500

    First commit

---
+++ src/shared/data.js
@@ -0,0 +1,37 @@
+import * as axios from 'axios';
+
+import { format } from 'date-fns';
+import { inputDateFormat } from './constants';
+
+import { API } from './config';
+
+const getHeroes=async function(){
+  try {
+    
+  
+    const response=await axios.get(`${API}/heroes.json`);
+    let data=parseList(response);
+    const heroes=data.map(h =>{
+      h.originDate=format(h.originDate, inputDateFormat);
+      return h;
+    });
+    return heroes;
+ } catch (error) {
+   console.error(error);
+   return [];
+ }
+};
+
+const parseList = response =>{
+  if(response.status!=200) throw Error(response.message);
+  if(!response.data) return [];
+  let list=response.data;
+  if(typeof list !='object'){
+    list=[];
+  }
+  return list;
+};
+
+export const data = {
+  getHeroes,
+};
